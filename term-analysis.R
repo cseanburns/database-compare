6d0e37eca3ed6cb2f294b617c601d719c9eace1c
cseanburns <sean.csb@gmail.com>
2020-04-15 11:04:57 -0400

    finished single term code

--- term-analysis.R
+++ term-analysis.R
@@ -9,10 +9,14 @@ library(tidyr)
 library(tidytext)
 library(ggplot2)
 
+### Section 1: Word frequencies ###
+# The following section examines single terms only; but this is misleading
+# because these are database terms or controlled terminology and so terms 
+# with more than one word need to be maintained in the analysis. Save that for 
+# Section 2.
+
 textdf <- tibble(dbterms)
 
-### Word frequencies ###
-# Examine single terms only
 textdf <- textdf %>% 
   unnest_tokens(word, Term)
 
@@ -20,24 +24,34 @@ textdf <- textdf %>%
 textdf <- textdf %>%
   anti_join(stop_words)
 
-# Chose n > 96 to get the top ten terms
+# expand data based on the Freq variable so that each observation is multiplied by its Freq count
+textdf <- textdf %>% uncount(Freq)
+
+# Chose n > 2870 to get the top ten terms
 textdf %>% 
   count(word, sort = TRUE) %>%
-  #filter(n > 96) %>%
+  filter(n > 2870) %>%
   mutate(word = reorder(word, n)) %>%
   ggplot(aes(word, n)) +
   geom_col() +
   xlab(NULL) +
+  coord_flip() + 
+  theme_bw() 
+
+# Choose n > 700 so that all databases are plotted
+textdf %>%
+  count(Database, word, sort = TRUE) %>%
+  filter(n > 700) %>%
+  group_by(Database) %>%
+  mutate(proportion = n / sum(n)) %>% 
+  ggplot(aes(word, proportion)) +
+  geom_col() +
+  xlab(NULL) +
   coord_flip() +
+  facet_wrap(~Database, ncol = 8) +
   theme_bw()
 
-textdf %>% 
-  count(Database, word) %>%
-  filter(n > 4) %>%
-  group_by(Database) %>%
-  mutate(proportion = n / sum(n)) %>%
-  ggplot(aes(word, proportion)) +
-  geom_abline() +
-  geom_jitter() +
-  geom_text(aes(label = word), check_overlap = TRUE, vjust = 1.5) +
-  facet_wrap(~Database)
+
+### Section 2: n-grams ###
+
+textdf <- tibble(dbterms)
