55bb0bb76271362dd9cbe931926d342aaa23fd54
cseanburns <sean.csb@gmail.com>
2020-05-07 17:20:24 -0400

    update for new data

--- data-prep.R
+++ data-prep.R
@@ -4,14 +4,13 @@ library(tidyr)
 library(tidytext)
 library(ggplot2)
 
-# setwd('/home/sean/Dropbox/workspace/database-compare/') # when I'm using NVim-R
 dbterms <- read.csv(file = "data/data.csv", header = TRUE, sep = "\t")
 
 # convert terms to characters
 dbterms$Term <- as.character(dbterms$Term)
 
-# Merge 1911 with 1910 and 1982 with 1980 start years
-dbterms$StartYear[dbterms$StartYear == 1911] <- 1910
-dbterms$StartYear[dbterms$StartYear == 1982] <- 1980
+# Merge 1886 with 1880
+dbterms$StartYear[dbterms$StartYear == 1886] <- 1880
 # Merge odd year out with 1999 end year
-dbterms$EndYear[dbterms$EndYear == 1991] <- 1999
+dbterms$EndYear[dbterms$EndYear == 1920] <- 1929
+dbterms$EndYear[dbterms$EndYear == 1981] <- 1989
