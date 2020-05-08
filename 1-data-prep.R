3527a5ef8d301cac252e5bf73c594716e12d0436
cseanburns <sean.csb@gmail.com>
2020-05-08 11:09:44 -0400

    updated code for new data

--- 1-data-prep.R
+++ 1-data-prep.R
@@ -10,8 +10,11 @@ dbterms <- read.csv(file = "data/data.csv", header = TRUE, sep = "\t")
 dbterms$Term <- as.character(dbterms$Term)
 dbterms$Freq <- as.integer(dbterms$Freq)
 
-# Merge 1886 with 1880
+# Assign years to start of decade
 dbterms$StartYear[dbterms$StartYear == 1886] <- 1880
-# Merge odd year out with 1999 end year
+dbterms$StartYear[dbterms$StartYear == 1982] <- 1980
+
+# Assign years to end of decade
 dbterms$EndYear[dbterms$EndYear == 1920] <- 1929
 dbterms$EndYear[dbterms$EndYear == 1981] <- 1989
+
--- 2-basic-analysis.R
+++ 2-basic-analysis.R
@@ -2,17 +2,16 @@
 
 library("RColorBrewer") # for plot colors
 
-source("data-prep.R")
-
 # Calculate the total number of terms by decade
 yearsterms <- as.table(tapply(dbterms$Freq, dbterms$StartYear, FUN = sum))
 # Convert to data frame
 yearstermsdb <- data.frame(yearsterms)
+rm(yearsterms)
 names(yearstermsdb) <- c("Decade", "Frequency")
 yearstermsdb$Decade <- as.Date(yearstermsdb$Decade, "%Y")
 
 # Line plot total number of terms by decade
-# jpeg('plots/growth.jpg', width = 3840, height = 2160, pointsize = 12, res = 300)
+#jpeg('plots/growth.jpg', width = 3840, height = 2160, pointsize = 12, res = 300)
 plot(yearstermsdb$Decade, yearstermsdb$Frequency,
   type = "b",
   xlab = "Decades",
@@ -24,78 +23,82 @@ text(yearstermsdb$Decade, yearstermsdb$Frequency,
   offset = 1,
   labels = yearstermsdb$Frequency
 )
-# dev.off()
+#dev.off()
 
 # Focus on frequency of database, weighted by frequency of terms per database
 plotcolors <- brewer.pal(8, "Spectral")
 
-weightedDB <- count(dbterms, vars = "Database", wt_var = "Freq")
+weightedDB <- plyr::count(dbterms, vars = "Database", wt_var = "Freq")
 names(weightedDB) <- c("Database", "Term_Frequency")
-weightedDB$Percentage <- round(weightedDB$Term_Frequency / sum(dbterms$Freq) * 100, 2)
+weightedDB$Percentage <- round(weightedDB$Term_Frequency 
+                               / sum(dbterms$Freq) * 100, 2)
 
-# jpeg('plots/weightedDB.jpg', width = 3840, height = 2160, pointsize = 12, res = 300)
-barplot(weightedDB$Term_Frequency,
+#jpeg('plots/weightedDB.jpg', width = 3840, height = 2160, pointsize = 12, res = 300)
+barplot(weightedDB$Percentage,
   col = plotcolors,
-  names.arg = weightedDB$Database
+  names.arg = weightedDB$Database,
+  xlab = "Database",
+  ylab = "Percentage",
+  las = 2
 )
-# dev.off()
+#dev.off()
 
 # Set up barplots by decade and frequency of terms per database
-weightedDB1890 <- subset(dbterms, subset = dbterms$StartYear == 1890)
-weightedDB1890 <- count(weightedDB1890, vars = "Database", wt_var = "Freq")
+weightedDB1890 <- subset(dbterms, subset = dbterms$StartYear == 1880)
+weightedDB1890 <- plyr::count(weightedDB1890, vars = "Database", wt_var = "Freq")
 weightedDB1890$Decade <- "1890"
 
 weightedDB1900 <- subset(dbterms, subset = dbterms$StartYear == 1900)
-weightedDB1900 <- count(weightedDB1900, vars = "Database", wt_var = "Freq")
+weightedDB1900 <- plyr::count(weightedDB1900, vars = "Database", wt_var = "Freq")
 weightedDB1900$Decade <- "1900"
 
 weightedDB1910 <- subset(dbterms, subset = dbterms$StartYear == 1910)
-weightedDB1910 <- count(weightedDB1910, vars = "Database", wt_var = "Freq")
+weightedDB1910 <- plyr::count(weightedDB1910, vars = "Database", wt_var = "Freq")
 weightedDB1910$Decade <- "1910"
 
 weightedDB1920 <- subset(dbterms, subset = dbterms$StartYear == 1920)
-weightedDB1920 <- count(weightedDB1920, vars = "Database", wt_var = "Freq")
+weightedDB1920 <- plyr::count(weightedDB1920, vars = "Database", wt_var = "Freq")
 weightedDB1920$Decade <- "1920"
 
 weightedDB1930 <- subset(dbterms, subset = dbterms$StartYear == 1930)
-weightedDB1930 <- count(weightedDB1930, vars = "Database", wt_var = "Freq")
+weightedDB1930 <- plyr::count(weightedDB1930, vars = "Database", wt_var = "Freq")
 weightedDB1930$Decade <- "1930"
 
 weightedDB1940 <- subset(dbterms, subset = dbterms$StartYear == 1940)
-weightedDB1940 <- count(weightedDB1940, vars = "Database", wt_var = "Freq")
+weightedDB1940 <- plyr::count(weightedDB1940, vars = "Database", wt_var = "Freq")
 weightedDB1940$Decade <- "1940"
 
 weightedDB1950 <- subset(dbterms, subset = dbterms$StartYear == 1950)
-weightedDB1950 <- count(weightedDB1950, vars = "Database", wt_var = "Freq")
+weightedDB1950 <- plyr::count(weightedDB1950, vars = "Database", wt_var = "Freq")
 weightedDB1950$Decade <- "1950"
 
 weightedDB1960 <- subset(dbterms, subset = dbterms$StartYear == 1960)
-weightedDB1960 <- count(weightedDB1960, vars = "Database", wt_var = "Freq")
+weightedDB1960 <- plyr::count(weightedDB1960, vars = "Database", wt_var = "Freq")
 weightedDB1960$Decade <- "1960"
 
 weightedDB1970 <- subset(dbterms, subset = dbterms$StartYear == 1970)
-weightedDB1970 <- count(weightedDB1970, vars = "Database", wt_var = "Freq")
+weightedDB1970 <- plyr::count(weightedDB1970, vars = "Database", wt_var = "Freq")
 weightedDB1970$Decade <- "1970"
 
 weightedDB1980 <- subset(dbterms, subset = dbterms$StartYear == 1980)
-weightedDB1980 <- count(weightedDB1980, vars = "Database", wt_var = "Freq")
+weightedDB1980 <- plyr::count(weightedDB1980, vars = "Database", wt_var = "Freq")
 weightedDB1980$Decade <- "1980"
 
 weightedDB1990 <- subset(dbterms, subset = dbterms$StartYear == 1990)
-weightedDB1990 <- count(weightedDB1990, vars = "Database", wt_var = "Freq")
+weightedDB1990 <- plyr::count(weightedDB1990, vars = "Database", wt_var = "Freq")
 weightedDB1990$Decade <- "1990"
 
 weightedDB2000 <- subset(dbterms, subset = dbterms$StartYear == 2000)
-weightedDB2000 <- count(weightedDB2000, vars = "Database", wt_var = "Freq")
+weightedDB2000 <- plyr::count(weightedDB2000, vars = "Database", wt_var = "Freq")
 weightedDB2000$Decade <- "2000"
 
 weightedDB2010 <- subset(dbterms, subset = dbterms$StartYear == 2010)
-weightedDB2010 <- count(weightedDB2010, vars = "Database", wt_var = "Freq")
+weightedDB2010 <- plyr::count(weightedDB2010, vars = "Database", wt_var = "Freq")
 weightedDB2010$Decade <- "2010"
 
-# jpeg('plots/decadefreqbarplots.jpg', width = 3840, height = 2160, pointsize = 12, res = 300)
+jpeg('plots/decadefreqbarplots.jpg', width = 3840, height = 2160, pointsize = 12, res = 300)
 par(mfrow = c(4, 4))
-barplot(weightedDB1890$freq, names.arg = weightedDB1890$Database, las = 2, col = plotcolors, main = "1890s")
+barplot(weightedDB1890$freq, names.arg = weightedDB1890$Database, las = 2, col = plotcolors, main = "1880/90s")
 barplot(weightedDB1900$freq, names.arg = weightedDB1900$Database, las = 2, col = plotcolors, main = "1900s")
 barplot(weightedDB1910$freq, names.arg = weightedDB1910$Database, las = 2, col = plotcolors, main = "1910s")
 barplot(weightedDB1920$freq, names.arg = weightedDB1920$Database, las = 2, col = plotcolors, main = "1920s")
@@ -108,7 +111,7 @@ barplot(weightedDB1980$freq, names.arg = weightedDB1980$Database, las = 2, col =
 barplot(weightedDB1990$freq, names.arg = weightedDB1990$Database, las = 2, col = plotcolors, main = "1990s")
 barplot(weightedDB2000$freq, names.arg = weightedDB2000$Database, las = 2, col = plotcolors, main = "2000s")
 barplot(weightedDB2010$freq, names.arg = weightedDB2010$Database, las = 2, col = plotcolors, main = "2010s")
-# dev.off()
+dev.off()
 
 rm(
   weightedDB1890, weightedDB1900, weightedDB1910, weightedDB1920, weightedDB1930,
@@ -116,4 +119,4 @@ rm(
   weightedDB1990, weightedDB2000, weightedDB2010, weightedDB
 )
 rm(yearsterms, yearstermsdb)
-rm(plotcolors)
+rm(plotcolors, names.arg)
