ba70d05aed548e5dd66b4d7bd7d93614e566782b
cseanburns <sean.csb@gmail.com>
2020-05-13 17:09:42 -0400

    sorted

--- 0-load-libraries.R
+++ 0-load-libraries.R
@@ -1,6 +1,6 @@
-library("plyr") # for count function
 library("dplyr")
+library("ggplot2")
+library("plyr") # for count function
+library("RColorBrewer") # for plot colors
 library("tidyr")
 library("tidytext")
-library("ggplot2")
-library("RColorBrewer") # for plot colors
