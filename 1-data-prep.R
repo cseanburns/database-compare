1a16569f24a1b17b8c2e93562d277a26a7d707d3
cseanburns <sean.csb@gmail.com>
2020-06-08 09:47:48 -0400

    updated code for updated data

--- 1-data-prep.R
+++ 1-data-prep.R
@@ -6,10 +6,14 @@ dbterms <- read.csv(file = "data/data.csv", header = TRUE, sep = "\t")
 dbterms$Term <- as.character(dbterms$Term)
 dbterms$Freq <- as.integer(dbterms$Freq)
 
-# Assign years to start of decade
+# Check StartYears and assign years to start of decade
+table(dbterms$StartYear)
 dbterms$StartYear[dbterms$StartYear == 1886] <- 1880
 dbterms$StartYear[dbterms$StartYear == 1982] <- 1980
+table(dbterms$StartYear)
 
 # Assign years to end of decade
-dbterms$EndYear[dbterms$EndYear == 1920] <- 1929
+table(dbterms$EndYear)
 dbterms$EndYear[dbterms$EndYear == 1981] <- 1989
+dbterms$EndYear[dbterms$EndYear == 2008] <- 2009
+table(dbterms$EndYear)
